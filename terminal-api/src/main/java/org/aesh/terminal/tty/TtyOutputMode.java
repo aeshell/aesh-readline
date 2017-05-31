3c42226dab809717ea0198486e4abaf08f05f0b7
Ståle W. Pedersen <stalep@gmail.com>
2017-05-31 23:10:54 +0200

    fixed licence header

--- terminal-api/src/main/java/org/aesh/terminal/tty/TtyOutputMode.java
+++ terminal-api/src/main/java/org/aesh/terminal/tty/TtyOutputMode.java
@@ -1,5 +1,9 @@
 /*
- * Copyright 2015 Julien Viet
+ * JBoss, Home of Professional Open Source
+ * Copyright 2017 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +17,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.aesh.terminal.tty;
 
 import java.util.function.Consumer;
--- terminal-telnet/src/main/java/org/aesh/terminal/telnet/util/Helper.java
+++ terminal-telnet/src/main/java/org/aesh/terminal/telnet/util/Helper.java
@@ -1,8 +1,21 @@
 /*
- * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ * JBoss, Home of Professional Open Source
+ * Copyright 2017 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
  *
- * Licensed under the Eclipse Public License version 1.0, available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.aesh.terminal.telnet.util;
 
@@ -14,20 +27,20 @@ import java.util.function.Consumer;
  */
 public class Helper {
 
-  public static Consumer<Throwable> startedHandler(CompletableFuture<?> fut) {
-    return err -> {
-      if (err == null) {
-        fut.complete(null);
-      } else {
-        fut.completeExceptionally(err);
-      }
-    };
-  }
+    public static Consumer<Throwable> startedHandler(CompletableFuture<?> fut) {
+        return err -> {
+            if (err == null) {
+                fut.complete(null);
+            } else {
+                fut.completeExceptionally(err);
+            }
+        };
+    }
 
-  public static Consumer<Throwable> stoppedHandler(CompletableFuture<?> fut) {
-    return err -> {
-      fut.complete(null);
-    };
-  }
+    public static Consumer<Throwable> stoppedHandler(CompletableFuture<?> fut) {
+        return err -> {
+            fut.complete(null);
+        };
+    }
 
 }
